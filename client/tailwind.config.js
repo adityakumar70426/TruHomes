eda7539637bbd501af1420fc808bd788e32686cb
adityakumar70426 <adityakumar70426@gmail.com>
2025-11-26 13:06:41 +0530

    final commit

---
+++ client/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    // if you need other plugins, use ESM import at the top
+    // forms(),
+    // typography(),
+  ],
+}
